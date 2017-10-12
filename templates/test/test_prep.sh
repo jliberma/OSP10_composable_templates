9c000c1eb09e5491ac7e18d66d955b650ff77860
jliberma <jacobliberman@gmail.com>
2017-10-12 16:15:51 -0400

    fix stack templates

--- templates/test/test_prep.sh
+++ templates/test/test_prep.sh
@@ -9,9 +9,9 @@ curl http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img | opens
 # deploy the admin stack (creates project, user, networks)
 openstack stack create -t templates/test/admin_test.yaml admin_test
 
-# deploy the project stack (launches instance, installs cuda via OS::Heat::SoftwareConfig)
+# deploy the project stack (launches instances)
 sed -e 's/OS_USERNAME=admin/OS_USERNAME=user1/' -e 's/OS_PROJECT_NAME=admin/OS_PROJECT_NAME=tenant1/' -e 's/OS_PASSWORD=.*/OS_PASSWORD=redhat/' ~/overcloudrc > ~/user1.rc
 source ~/user1.rc
-openstack keypair create stack > stack.pem
-chmod 600 stack.pem
+openstack keypair create stack > ~/stack.pem
+chmod 600 ~/stack.pem
 openstack stack create -t templates/test/user_test.yaml user_test
