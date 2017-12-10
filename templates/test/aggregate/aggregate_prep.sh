ac6bdc8e4352c0866f42bb68f26f07eab300968f
jliberma <jacobliberman@gmail.com>
2017-12-09 19:01:40 -0500

    add aggregate test files

--- templates/test/aggregate/aggregate_prep.sh
+++ templates/test/aggregate/aggregate_prep.sh
@@ -3,18 +3,16 @@
 # Prepare admin tenant for Heat tests
 
 # prepare the overcloud
-source ~/overcloudrc
-curl http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img | openstack image create --disk-format qcow2 --container-format bare  --public cirros-0.3.4-x86_64
+#source ~/overcloudrc
+#curl http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img | openstack image create --disk-format qcow2 --container-format bare  --public cirros-0.3.4-x86_64
 
-# deploy the admin stack (creates project, user, networks)
-openstack stack create -t templates/test/admin_test.yaml admin_test
+# configure the test stack environment
+sed -e 's/OS_USERNAME=admin/OS_USERNAME=test_user/' -e 's/OS_PROJECT_NAME=admin/OS_PROJECT_NAME=test_tenant/' -e 's/OS_PASSWORD=.*/OS_PASSWORD=redhat/' ~/overcloudrc > ~/test_user.rc
+source ~/test_user.rc
+openstack keypair create test > ~/test.pem
+chmod 600 ~/test.pem
 
-# deploy the project stack (launches instances)
-sed -e 's/OS_USERNAME=admin/OS_USERNAME=user1/' -e 's/OS_PROJECT_NAME=admin/OS_PROJECT_NAME=tenant1/' -e 's/OS_PASSWORD=.*/OS_PASSWORD=redhat/' ~/overcloudrc > ~/user1.rc
-source ~/user1.rc
-openstack keypair create stack > ~/stack.pem
-chmod 600 ~/stack.pem
-openstack stack create -t templates/test/user_test.yaml user_test
+openstack stack create -t ~/templates/aggregate/test_stack.yaml test_stack
 
 # perform admin tasks
 source ~/overcloudrc
